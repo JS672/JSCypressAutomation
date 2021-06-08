9d9b5dc977833afd9a039182ec986d830a113558
Justin Shek <justinsh@dunnhumby.com>
2021-06-08 11:28:50 +0100

    Version bump from 7.2 to 7.5

---
+++ cypress/integration/examplesJS/Test1.js
@@ -0,0 +1,12 @@
+describe('My First Test', () => {
+    it('My first test case', () => {
+      expect(true).to.equal(false)
+    })
+
+    it('My second test case', () => {
+        expect(true).to.equal(true)
+      })
+
+
+
+})
