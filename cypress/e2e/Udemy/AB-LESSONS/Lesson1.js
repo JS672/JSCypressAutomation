270c7c50418816d9fdea8ae03f07ebc98e83ca0a
Justin Shek <justin.shek@dunnhumby.com>
2023-05-04 15:00:26 +0100

    Section 2 - Lesson 5-7 progress

---
+++ cypress/e2e/Udemy/AB-LESSONS/Lesson1.js
@@ -0,0 +1,36 @@
+//Hello World
+console.log("Hello World!")
+
+// Variables
+var firstName = "John"
+let lastName = "Smith"
+
+console.log (lastName)
+
+var age, dateOfBirth, sex
+age = "5"
+sex ="Male"
+console.log(age)
+age = "6"
+console.log(age)
+
+//Constants
+const occupation = "Engineer"
+console.log(occupation)
+
+//Data types
+
+//String
+var middleName = "David"
+
+//Number
+var ageOfBrother = 25
+
+//Boolean
+var isHeMarried = false
+
+//Null (no value)
+var yearsInMarriage = null
+
+//Undefined
+var numberOfCars = undefined
